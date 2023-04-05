b488a96efc639ee04ffa33487ddf7ef6ca594117
SangMin <sd50712321@gmail.com>
2023-04-05 14:44:34 +0900

    loki로 로그확인하는 것까지 확인

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ const express = require("express");
 const app = express();
 const client = require("prom-client");
 const collectDefaultMetrics = client.collectDefaultMetrics;
+const morgan = require("morgan");
+const rfs = require("rotating-file-stream");
+const path = require("path");
+const fs = require("fs");
+const logger = require("./logger");
+
+// 로그 디렉터리 생성
+const logDirectory = path.join(__dirname, "logs");
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+// 로그 스트림 설정
+const accessLogStream = rfs.createStream("node-app.log", {
+  interval: "1d",
+  path: logDirectory,
+});
+
+// morgan을 사용하여 로그 파일에 로그 기록
+app.use(morgan("combined", { stream: accessLogStream }));
 
 // 프로메테우스 기본 메트릭 수집
 collectDefaultMetrics({ timeout: 5000 });
@@ -36,5 +54,18 @@ app.get("/metrics", (req, res) => {
     });
 });
 
+app.get("/err-test", (req, res) => {
+  try {
+    throw new Error("일반 에러 발생");
+  } catch (err) {
+    logger.error("error: ", err);
+    res.status(500).send(err);
+  }
+});
+
 // ... 애플리케이션 코드
-app.listen(3001, () => console.log("Listening on port 3001"));
+app.listen(3001, () => {
+  console.log("Listening on port 3001");
+  accessLogStream.write("Listening on part 3001 \n");
+  logger.info("Listening on port 3001");
+});
---
+++ logger.js
@@ -0,0 +1,26 @@
+const winston = require("winston");
+
+const logFormat = winston.format.combine(
+  winston.format.colorize(),
+  winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+  winston.format.printf(({ timestamp, level, message }) => {
+    return `${timestamp} ${level}: ${message}`;
+  })
+);
+
+const logger = winston.createLogger({
+  level: "info",
+  format: logFormat,
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.File({ filename: "error.log", level: "error" })
+  );
+}
+
+module.exports = logger;
