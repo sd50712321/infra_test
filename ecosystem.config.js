1e7f0e28ac4abf8d94d05d36d8f541ade0e3a690
SangMin <sd50712321@gmail.com>
2023-04-10 13:45:10 +0900

    불필요한 로그파일 만드는거 삭제

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,19 +7,13 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
-      env: {
-        NODE_ENV: "development",
-      },
-      env_production: {
-        NODE_ENV: "production",
-      },
       // pm2-metrics 모니터링 추가
       pmx: true,
       exp_backoff_restart_delay: 100,
       source_map_support: true,
       max_restarts: 10,
-      out_file: "./logs/console.log",
-      error_file: "./logs/console.log",
+      //   out_file: "./logs/console.log",
+      //   error_file: "./logs/console.log",
       merge_logs: true,
       time: true,
     },
