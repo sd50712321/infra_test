e243bd6dc13ee793ac8fedbfc628fc8898735d50
SangMin <sd50712321@gmail.com>
2023-04-11 19:37:50 +0900

    pm2 설정까지 모두 완료

--- app.js
+++ app.js
@@ -1,27 +1,49 @@
 const express = require("express");
 const app = express();
-const client = require("prom-client");
-const collectDefaultMetrics = client.collectDefaultMetrics;
 const path = require("path");
 const fs = require("fs");
 require("dotenv").config();
 const logger = require("./logger");
+const { sequelize, User } = require("./models");
+const metricsMiddleware = require("./middlewares/metricMiddlewares");
+const getMetrics = require("./middlewares/metricPm2Middlewares");
+const responseLoggingMiddleware = require("./middlewares/responseLoggingMiddleware");
+const client = require("prom-client");
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics({ timeout: 5000 });
+const { execSync } = require("child_process");
+
+let containerName = "";
+try {
+  containerName = execSync(
+    "cat /proc/self/cgroup | grep 'docker' | sed 's/^.*\\///' | tail -n1"
+  )
+    .toString()
+    .trim();
+} catch (error) {
+  logger.error("Could not get container name:", error);
+}
+
+(async () => {
+  await sequelize.sync();
+  // 여기에 초기 데이터를 삽입하거나 기존 데이터베이스와 동기화합니다.
+  for (let i = 0; i < 10; i++) {
+    await User.create({
+      name: `User ${i}`,
+      email: `user${i}@example.com`,
+    });
+  }
+})();
 
 // 로그 디렉터리 생성
 const logDirectory = path.join(__dirname, "logs");
 fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
-// 프로메테우스 기본 메트릭 수집
-collectDefaultMetrics({ timeout: 5000 });
-
-// HTTP 요청 메트릭 정의
-const httpRequestDuration = new client.Histogram({
-  name: "http_request_duration_seconds",
-  help: "HTTP request duration in seconds",
-  labelNames: ["method", "path", "status_code"],
-  buckets: [0.1, 0.3, 0.5, 1, 3, 5],
-});
-
+app.use(express.json());
+// 미들웨어로 메트릭 수집
+// app.use(metricsMiddleware);
+app.use(metricsMiddleware(containerName));
+app.use(responseLoggingMiddleware);
 app.use((req, res, next) => {
   if (req.url === "/metrics") {
     return next();
@@ -44,17 +66,9 @@ app.use((req, res, next) => {
         method: req.method,
         url: req.url,
         status: res.statusCode,
+        response: res.body, // 응답 객체를 response 키에 추가
       }
     );
-  });
-  next();
-});
-
-// 미들웨어로 메트릭 수집
-app.use((req, res, next) => {
-  const end = httpRequestDuration.startTimer();
-  res.on("finish", () => {
-    end({ method: req.method, path: req.path, status_code: res.statusCode });
   });
   next();
 });
@@ -68,18 +82,23 @@ app.use((err, req, res, next) => {
 });
 
 // 프로메테우스 메트릭 엔드포인트
-app.get("/metrics", (req, res) => {
-  res.set("Content-Type", client.register.contentType);
-  client.register
-    .metrics()
-    .then((metrics) => {
-      res.end(metrics);
-    })
-    .catch((err) => {
-      res.status(500).end(err);
-    });
-});
 
+app.get("/metrics", async (req, res) => {
+  try {
+    // 애플리케이션 메트릭을 가져옵니다.
+    const appMetrics = await client.register.metrics();
+    // PM2 메트릭을 가져옵니다.
+    const pm2Metrics = await getMetrics();
+    // 애플리케이션 메트릭과 PM2 메트릭을 결합합니다.
+    const combinedMetrics = appMetrics + "\n" + pm2Metrics;
+
+    // 결합한 메트릭을 응답으로 반환합니다.
+    res.set("Content-Type", "text/plain");
+    res.send(combinedMetrics);
+  } catch (err) {
+    res.status(500).send(err.toString());
+  }
+});
 app.get("/err-test", (req, res) => {
   try {
     const data = {
@@ -105,8 +124,75 @@ app.get("/err-test", (req, res) => {
   }
 });
 
+// 데이터베이스에서 데이터를 검색하는 예제
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (err) {
+    logger.error({ message: err.message, status: 500 });
+    res.status(500).send("데이터베이스 오류");
+  }
+});
+
+app.post("/users", async (req, res) => {
+  try {
+    // Create a new user object using the request body
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+    });
+
+    // Send the new user object back in the response
+    res.status(201).json(newUser);
+  } catch (error) {
+    // Handle any errors that occur during user creation
+    logger.error(error);
+    res.status(500).json({ message: "Error creating user" });
+  }
+});
+
+app.put("/users/1", async (req, res) => {
+  // 예를 들어, 사용자의 이메일을 업데이트하려고 한다고 가정합니다.
+  const updatedEmail = req?.body?.email;
+
+  try {
+    const user = await User.findOne({ where: { id: 1 } });
+
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    user.email = updatedEmail;
+    await user.save();
+
+    res
+      .status(200)
+      .send({ message: "User 1 email updated", email: updatedEmail });
+  } catch (error) {
+    logger.error(error);
+    res.status(500).send({ message: "Error updating user email" });
+  }
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  if (reason instanceof Error) {
+    logger.error(
+      `Unhandled Rejection at: ${JSON.stringify(promise)}, reason: ${
+        reason.stack
+      }`
+    );
+  } else {
+    logger.error(
+      `Unhandled Rejection at: ${JSON.stringify(
+        promise
+      )}, reason: ${JSON.stringify(reason)}`
+    );
+  }
+});
+
 // ... 애플리케이션 코드
 app.listen(3001, () => {
-  console.log("Listening on port 3001");
+  //   logger.log("Listening on port 3001");
   logger.info("Listening on port 3001");
 });
